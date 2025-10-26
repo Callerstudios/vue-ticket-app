55854e68b88663b557fd824474fd10d7ee557d75
DESKTOP-8AR3IAJ\USER PC <busariroqeeb16@gmail.com>
2025-10-26 09:12:46 +0100

    feat: add Header and Footer components with layout and styling

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,30 @@
 <script setup>
-import { RouterView } from 'vue-router'; </script>
+import { RouterView } from "vue-router";
+import Header from "./components/Header.vue";
+import Footer from "./components/Footer.vue";
+</script>
 
 <template>
-  <RouterView/>
+  <body>
+    <Header />
+    <RouterView />
+    <Footer />
+  </body>
 </template>
 
-<style scoped></style>
+<style>
+/* ===== Base Reset ===== */
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  scroll-behavior: smooth;
+  font-family: "Inter", Arial, sans-serif;
+  background-color: #f9fafb;
+  color: #111;
+  line-height: 1.5;
+}
+</style>
